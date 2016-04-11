76729d69643e850a6859801e5aadf0a23f015fd2
manjuprasadsn <manju.mpsn@gmail.com>
2016-04-11 10:07:45 +0530

    Updated

--- Graphics/rectmesh/mesh.c
+++ Graphics/rectmesh/mesh.c
@@ -35,11 +35,9 @@ void disp()
 void main(int argc,char **argv)
 {
   glutInit(&argc,argv);
-  glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
   glutInitWindowSize(800,600);
-  glutInitWindowPosition(0,0);
   glutCreateWindow("Rectangular Mesh");
-  glutDisplayFunc(disp);
   init();
+  glutDisplayFunc(disp);
   glutMainLoop();
 }
